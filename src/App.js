620079bd8bd775c6d49c5d3dcc80dbfb6404d275
William Bogans <williambogans98@yahoo.com>
2021-03-30 12:50:11 -0400

    Adds new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function getUser() {
+  return Promise.resolve({ id: '1', name: 'Robin' });
+}
 
 function App() {
+  const [search, setSearch] = React.useState('');
+
+  const [user, setUser] = React.useState(null);
+ 
+  React.useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+ 
+    loadUser();
+  }, []);
+ 
+ 
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+            {user ? <p>Signed in as {user.name}</p> : null}
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+ 
+      <p>Searches for {search ? search : '...'}</p>
     </div>
   );
 }
-
+ 
+export function Search({ value, onChange, children }) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+ 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,69 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { Search } from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import userEvent from '@testing-library/user-event';
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+ 
+    screen.getByText('Search:');
+    expect(screen.getByText('Search:')).toBeInTheDocument();
+    expect(screen.getByText(/Search:/)).toBeInTheDocument();
+  });
+
+  it("gets the searchbox by role", () => {
+    render(<App/>)
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  })
+
+  it("should fail since text is not in the document", () => {
+    render(<App/>)
+    // We use queryBy* here since getByText fails before hitting the assertion
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+  })
+
+  it("should find text after a fetching some data", async () => {
+    render(<App/>)
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+  })
+
+
+  it("should have the right value after fireEvent is called", async () => {
+    render(<App/>)
+    await screen.findByText(/Signed in as/);
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' },
+    });
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+  })
+
+
+
+  it("should have the right value after userEvent is called", async () => {
+    render(<App/>)
+    await screen.findByText(/Signed in as/);
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+  })
+});
+
+
+describe('Search', () => {
+  test('calls the onChange callback handler', async () => {
+    const onChange = jest.fn();
+ 
+    render(
+      <Search value="" onChange={onChange}>
+        Search:
+      </Search>
+    );
+ 
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+ 
+    expect(onChange).toHaveBeenCalledTimes(10);
+  });
 });
